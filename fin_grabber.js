e4872f0ffd7960c387f712f7b44aa583f8a5b8ce
Manuel Esberger <esberger.manuel@live.at>
2018-12-23 21:15:23 +0100

    finished categorizer

--- fin_grabber.js
+++ fin_grabber.js
@@ -34,15 +34,13 @@ if (on) {
           let day = dayEntry.find(".datecol .day").text();
           if (!data[month][day]) data[month][day] = [];
           let parsedDayEntry = parseDayEntry(dayEntry);
-          console.log(parsedDayEntry);
           if (parsedDayEntry) {
             data[month][day].push(parsedDayEntry);
           }
         });
       }
     });
-    writeBack(data);
-    // console.log(JSON.stringify(data, null, 2));
+    data = writeBack(data);
     buildCategoryFile(data);
   });
 }
@@ -71,82 +69,159 @@ function categorizeData(categories, data) {
           deasync.sleep(100);
         }
         elem = data[month][day][index];
-        let param = false;
-        let cat = {};
-        let updateCategory;
-        if (+elem.amount > 0) {
-          param = true;
-          cat = categories.in;
-          updateCategory = category => {
-            categories.in = category;
-          };
-        } else {
-          param = false;
-          cat = categories.out;
-          updateCategory = category => {
-            categories.out = category;
-          };
+
+        if (month === "Oktober2018" && day === "31") {
+          console.log(JSON.stringify(elem));
         }
-        getCategory(param, cat, elem).then(category => {
-          updateCategory(category);
-          fs.writeFile(categoryFile, JSON.stringify(categories), () => {});
-        });
+
+        if (elem.category) {
+          console.log(elem.info + " is an exception");
+        } else {
+          let param = false;
+          let cat = {};
+          let updateCategory;
+          if (+elem.amount > 0) {
+            param = true;
+            cat = categories.in;
+            updateCategory = category => {
+              categories.in = category;
+            };
+          } else {
+            param = false;
+            cat = categories.out;
+            updateCategory = category => {
+              categories.out = category;
+            };
+          }
+          getCategory(param, cat, elem).then(category => {
+            updateCategory(category);
+            fs.writeFile(categoryFile, JSON.stringify(categories), () => {});
+          });
+        }
       }
     });
   });
 }
 
 async function getCategory(inFlow, categories, elem) {
+  let rootCat = inFlow ? "in" : "out";
+  let categoryText = "";
   if (inFlow) {
-    console.log("Incoming category " + elem.info + " (" + elem.amount + "€)");
+    categoryText =
+      " (+) Incoming entry " + elem.info + " (" + elem.amount + "€)\n";
   } else {
-    console.log("Outgoing category " + elem.info + " (" + elem.amount + "€)");
+    categoryText =
+      " (-) Outgoing entry " + elem.info + " (" + elem.amount + "€)\n";
   }
-  if (!categoryContainsValue(categories, elem.info)) {
-    answer = await inquireCategory("", categories, elem);
-    return answer;
-  } else {
+
+  elem.info = elem.info.toUpperCase();
+  const listCategories = flatten(categories);
+
+  if (listCategories.includes(elem.info)) {
     return categories;
+  } else if (listCategories.length > 0) {
+    console.log("");
+    const bestMatch = stringSimilarity.findBestMatch(elem.info, listCategories)
+      .bestMatch;
+    if (bestMatch.rating > 0.5) {
+      const categoryPath = getCategoryPath(
+        rootCat,
+        bestMatch.target,
+        categories
+      );
+
+      const shouldUse = await inquirePreCategory(
+        categoryText,
+        bestMatch,
+        categoryPath
+      );
+      categoryText = "";
+      if (shouldUse) {
+        return addToCategory(
+          categoryPath.split(".").slice(1),
+          categories,
+          elem.info
+        );
+      }
+    }
+  }
+  answer = await inquireCategory(categoryText, "", categories, elem);
+  return answer;
+}
+
+function addToCategory(categoryPath, category, info) {
+  let returnValue = category;
+  if (categoryPath.length > 0) {
+    let smallerParts = categoryPath.slice(1);
+    returnValue[categoryPath[0]] = addToCategory(
+      smallerParts,
+      returnValue[categoryPath[0]],
+      info
+    );
+    return returnValue;
+  } else {
+    returnValue.push(info);
+    return returnValue;
   }
 }
 
-async function inquireCategory(path, categories, elem) {
+async function inquirePreCategory(categoryText, bestMatch, categoryPath) {
+  inquirerLock = true;
+
+  let answers = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "selection",
+      message:
+        categoryText +
+        "Similar entry " +
+        bestMatch.target +
+        " (" +
+        bestMatch.rating +
+        ") found. Sould the new entry be placed in path: " +
+        categoryPath.split(".").join(" > ")
+    }
+  ]);
+
+  inquirerLock = false;
+  return answers.selection;
+}
+
+async function inquireCategory(categoryText, path, categories, elem) {
   const placeHere = "place here";
   const newCategory = "new category";
   const goBack = "go back";
+  const skip = "skip";
 
   inquirerLock = true;
   returnValue = false;
 
-  let choices = [];
-  if (Array.isArray(categories)) {
-    choices = [placeHere];
-    choices.push(newCategory);
-    if (!isEmpty(path)) {
-      choices.push(goBack);
-    }
-  } else {
-    let keys = Object.keys(categories);
-    choices = [...keys, new inquirer.Separator(), newCategory];
-    if (!isEmpty(path)) {
-      choices.push(goBack);
-    }
-    choices.push(new inquirer.Separator());
-  }
+  let choices = prepareChoises(
+    path,
+    placeHere,
+    newCategory,
+    skip,
+    goBack,
+    categories
+  );
+
   while (!returnValue) {
     let answers = await inquirer.prompt([
       {
         type: "list",
         name: "selection",
         message:
-          "Where do you want to place it? " +
-          (isEmpty(path) ? "" : "\n" + path + " > ..."),
+          categoryText +
+          (isEmpty(path) ? "Where do you want to place it? " : path + " > ..."),
         choices
       }
     ]);
+    categoryText = "";
 
     if (answers.selection === goBack) {
       return false;
+    } else if (answers.selection === skip) {
+      returnValue = true;
     } else if (answers.selection === placeHere) {
       categories.push(elem.info);
       returnValue = true;
@@ -171,11 +246,12 @@ async function inquireCategory(path, categories, elem) {
         answers.selection = newCat.new;
       }
       returnValue = await inquireCategory(
+        "",
         (isEmpty(path) ? "" : path + " > ") + answers.selection,
         categories[answers.selection],
         elem
       );
-      if (returnValue) {
+      if (typeof returnValue === "object") {
         categories[answers.selection] = returnValue;
       }
     }
@@ -184,22 +260,40 @@ async function inquireCategory(path, categories, elem) {
   return categories;
 }
 
-function categoryContainsValue(category, insertValue) {
+function prepareChoises(
+  path,
+  placeHere,
+  newCategory,
+  skip,
+  goBack,
+  categories
+) {
+  let choices = [];
+  if (Array.isArray(categories)) {
+    choices = [placeHere, newCategory, skip];
+    if (!isEmpty(path)) {
+      choices.push(goBack);
+    }
+  } else {
+    let keys = Object.keys(categories);
+    choices = [...keys, new inquirer.Separator(), newCategory, skip];
+    if (!isEmpty(path)) {
+      choices.push(goBack);
+    }
+    choices.push(new inquirer.Separator());
+  }
+  return choices;
+}
+
+function getCategoryPath(path, findValue, categories) {
   let returnValue = false;
-  if (category) {
-    Object.keys(category).forEach(function(key, index) {
-      const value = category[key];
-      // console.log("Key " + key + "; Value " + value);
-      if (value === insertValue) {
-        console.log("value " + value + " already in category");
-        returnValue = true;
-        return;
-      } else if (typeof value === "object") {
-        if (categoryContainsValue(value, insertValue)) {
-          returnValue = true;
-          return;
-        }
-      }
+  if (Array.isArray(categories) && categories.includes(findValue)) {
+    return path;
+  } else if (typeof categories === "object") {
+    Object.keys(categories).forEach(function(key) {
+      const value = categories[key];
+      let newValue = getCategoryPath(path + "." + key, findValue, value);
+      if (newValue) returnValue = newValue;
     });
   }
   return returnValue;
@@ -209,26 +303,22 @@ function writeBack(data) {
   if (fs.existsSync(dataFile)) {
     //append
     //@see https://stackoverflow.com/questions/36856232/write-add-data-in-json-file-using-node-js
-    fs.readFile(dataFile, "utf8", function readFileCallback(err, previousData) {
-      if (err) {
-        console.log(err);
-      } else {
-        obj = JSON.parse(previousData); //now it an object
-        obj = { ...obj, ...data };
-        json = JSON.stringify(obj); //convert it back to json
-        fs.writeFile(dataFile, json, "utf8"); // write it back
-      }
-    });
+    previousData = fs.readFileSync(dataFile, "utf8");
+    obj = JSON.parse(previousData); //now it an object
+    obj = { ...data, ...obj };
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile(dataFile, json, "utf8", () => {}); // write it back
+    return obj;
   } else {
     //overwrite
-    fs.writeFile(dataFile, JSON.stringify(data), "utf8");
+    fs.writeFile(dataFile, JSON.stringify(data), "utf8", () => {});
   }
 }
 
 function parseDayEntry(dayEntry) {
   let data = {};
 
-  data.info = getText(dayEntry.find(".accountcol h5"));
+  data.info = getText(dayEntry.find(".accountcol h5")).toUpperCase();
   data.amount = getText(dayEntry.find(".amountcol .balance")).replace(",", ".");
 
   if (isNaN(data.amount) || +data.amount == 0) return false; // *** Abschlussbuchung per 30.09.2018 **** interessiert uns nicht
@@ -238,7 +328,7 @@ function parseDayEntry(dayEntry) {
 function getText(elem) {
   return elem
     .text()
-    .replace(/(\r\n\t|\n|\r\t)/gm, "")
+    .replace(/(\r\n\t|\n|\r\t|\.)/gm, "")
     .trim();
 }
 
@@ -269,4 +359,16 @@ function isEmpty(obj) {
   }
 
   return true;
+}
+
+function flatten(category) {
+  let returnValue = [];
+  if (Array.isArray(category)) {
+    return category;
+  } else {
+    Object.keys(category).forEach(key => {
+      returnValue.push(...flatten(category[key]));
+    });
+  }
+  return returnValue;
 }
