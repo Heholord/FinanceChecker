92f99d2cb93a24883f3cd4455f2a37ed92b6105e
MANUEL Esberger <esberger.manuel@live.at>
2019-07-08 14:01:57 +0200

    organize todo comments

--- finance-checker-client/src/view/management/DataManagement.vue
+++ finance-checker-client/src/view/management/DataManagement.vue
@@ -3,29 +3,28 @@
     <!-- TODO  
        1) html and/or json (make hirachy clear) 
        2) desicion component for:
-          -> upload existing data
-            -> add new data
-              -> entry browser, category browser, ect
-            -> entry browser, category browser, ect
-          -> upload bank data (html, csv)
-            -> usual flow
+          -> upload (q: do you have some data to upload? a: upload data, take a tour)
+            -> upload existing data
+              -> then add new data
+            -> upload bank data (html, csv)
+              -> select bank:
+              -> select country, then list banks in choice component with unified logo style
+              -> add new data
+              -> choose merge strategy in choice component (newest, merge non empty days, merge months, merge years, merge everything)]
+                -> name side effects of each merge strategy (maybe in bulletpoints)
+                -> explain what it does (i.e: creates new days, extends days, extends months)
+                -> maybe make negative and positive bulletpoints
+            -> then entry browser, category browser, ect
+          -> take a tour
         
       3)
-      [first upload your existing data (q: do you have existing data? a: upload existing, continue)
-      select bank
-      add new data
-      merge strategy (newest, merge non empty days, merge months, merge years, merge everything)]
-        -> name side effects of each merge strategy (maybe in bulletpoints)
-        -> explain what it does (i.e: creates new days, extends days, extends months)
-
-
       4) options(delete, add, modify, special category) in entity view 
       5) make a finish page (with nice animations)
       6) jump to finish page option when store data and category exits
       7) loading from upload to entity browser does not work correctly (maybe split methods?). check if fixed
     -->
     <heading heading="Manage your data" img="manage-i.svg"></heading>
-    <root-nav/>
+    <root-nav />
     <div class="width-center">
       <div class="center-content">
         <div class="tabs">
@@ -45,8 +44,8 @@
             @selected="key => this.selected = key"
           />
           <div class="content">
-            <Download v-if="selected === 'download'"/>
-            <EntryBrowser v-if="selected === 'edit'"/>
+            <Download v-if="selected === 'download'" />
+            <EntryBrowser v-if="selected === 'edit'" />
             <div v-if="selected === 'upload'">
               <choices v-if="$route.path === '/manage'" :choices="uploadChoices" @select="route"></choices>
               <router-view></router-view>
