8225a398bddbeaf08948dd0c493eee3b965a10bd
MANUEL Esberger <esberger.manuel@live.at>
2019-07-10 13:07:22 +0200

    new demo mode implemented

--- finance-checker-client/src/view/RootView.vue
+++ finance-checker-client/src/view/RootView.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="root contentView">
-    <heading heading="Finance Checker"/>
+    <heading heading="Finance Checker" />
     <div class="width-center h100 w100">
-      <div class="center-content">
+      <div v-if="!$store.getters.hasData" class="tabs">
+        <choices
+          :choices="emptyWorkspaceChoices"
+          title="You have a clean workspace. Let's change that..."
+          @select="execute"
+          big
+        ></choices>
+      </div>
+      <div v-else class="tabs">
         <choices :choices="choices" big @select="execute"></choices>
       </div>
     </div>
@@ -12,16 +20,32 @@
 <script>
 import Choices from "@/components/Choices";
 import Heading from "@/components/Heading";
+import { convert } from "@/plugin/utils.js";
 
 export default {
   name: "RootView",
   components: { Choices, Heading },
   data() {
     return {
+      emptyWorkspaceChoices: [
+        {
+          text: "Upload transaction data",
+          subtext: "and start the journey",
+          image: "manage.svg",
+          route: "/manage"
+        },
+        {
+          text: "Launche the demo",
+          subtext: "and get to know the environment",
+          image: "test.svg",
+          dummy: true,
+          route: "/visualize"
+        }
+      ],
       choices: [
         {
-          text: "Manage your data",
-          subtext: "and start your journey",
+          text: "Upload data from the bank",
+          subtext: "and proceede the journey",
           image: "manage.svg",
           route: "/manage"
         },
@@ -36,7 +60,18 @@ export default {
   },
   methods: {
     execute(choice) {
-      this.$routeTo(choice.route);
+      if (choice.dummy) this.uploadDummy();
+      if (choice.route) this.$routeTo(choice.route);
+    },
+    uploadDummy() {
+      let mergeEntries = require("@/assets/data.demo.json");
+      this.updateStore(mergeEntries);
+    },
+    updateStore(data, doneAction) {
+      data = convert(data);
+      this.$store.dispatch("setData", data).then(() => {
+        if (doneAction) doneAction();
+      });
     }
   }
 };
